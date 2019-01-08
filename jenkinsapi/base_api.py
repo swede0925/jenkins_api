eae7bf2f7f7705cbe1b620e52b58db6af44bb385
shijialiang <shijialiang@wdai.com>
2019-01-08 15:19:00 +0800

    update

--- jenkinsapi/base_api.py
+++ jenkinsapi/base_api.py
@@ -132,8 +132,8 @@ class jenkinsapi:
         :param depth:
         :return:
         """
-        JOB_INFO_ENDPOINT = 'job/%s/api/json?depth=%s' % (job_name, depth)
-        resp = self.http_get(JOB_INFO_ENDPOINT)
+        JOB_INFO_ENDPOINT = 'job/%s/api/json' % job_name
+        resp = self.http_get(JOB_INFO_ENDPOINT, query_data={'depth': depth})
         return resp.json()
 
     def enable_job(self, job_name):
@@ -156,7 +156,7 @@ class jenkinsapi:
 
     def get_job_config(self, job_name):
         """
-        Get configuration of existing Jenkins job.
+        获取Jenkins job配置
         :param job_name:
         :return:
         """
@@ -166,7 +166,7 @@ class jenkinsapi:
 
     def reconfig_job(self, job_name, config_xml):
         """
-        Change configuration of existing Jenkins job.
+        改变Jenkins job配置
         :param job_name:
         :param config_xml:
         :return:
@@ -192,6 +192,26 @@ class jenkinsapi:
         """
         DELETE_JOB_ENDPOINT = 'job/%s/doDelete' % job_name
         self.http_post(DELETE_JOB_ENDPOINT)
+
+    def rename_job(self, from_name, to_name):
+        """
+        重命名jenkins job
+        :param from_name:
+        :param to_name:
+        :return:
+        """
+        RENAME_JOB_ENDPOINT = 'job/%s/doRename' % from_name
+        self.http_post(RENAME_JOB_ENDPOINT, query_data={'newName': to_name})
+
+    def copy_job(self, from_name, to_name):
+        """
+        复制jenkins job
+        :param from_name:
+        :param to_name:
+        :return:
+        """
+        COPY_JOB_ENDPOINT = 'createItem'
+        self.http_post(COPY_JOB_ENDPOINT, query_data={'from': from_name, 'name': to_name, 'mode': 'copy'})
 
     def get_info(self):
         """
@@ -238,8 +258,8 @@ class jenkinsapi:
         :param view_name:
         :return:
         """
-        VIEW_JOBS_ENDPOINT = 'view/%s/api/json?tree=jobs[url,color,name]' % view_name
-        resp = self.http_get(VIEW_JOBS_ENDPOINT)
+        VIEW_JOBS_ENDPOINT = 'view/%s/api/json' % view_name
+        resp = self.http_get(VIEW_JOBS_ENDPOINT, query_data={'tree': 'jobs[url,color,name]'})
         return resp.json()['jobs']
 
     def get_view_config(self, view_name):
@@ -283,12 +303,90 @@ class jenkinsapi:
 
     def get_build_info(self, job_name, build_number, depth=0):
         """
-        获取job第number次信息
+        获取Jenkins build的信息
         :param job_name:
         :param build_number:
         :param depth:
         :return:
         """
-        BUILD_INFO_ENDPOINT = 'job/%s/%d/api/json?depth=%s' % (job_name, build_number, depth)
-        resp = self.http_get(BUILD_INFO_ENDPOINT)
+        BUILD_INFO_ENDPOINT = 'job/%s/%d/api/json' % (job_name, build_number)
+        resp = self.http_get(BUILD_INFO_ENDPOINT, query_data={'depth': depth})
         return resp.json()
+
+    def build_job(self, job_name, parameters=None):
+        """
+        触发jenkins job,可以带参数
+        :param name:
+        :param parameters: parameters for job, or ``None``, ``dict``
+        :param token:
+        :return:
+        """
+        BUILD_JOB_ENDPOINT = 'job/%s/build' % job_name
+        BUILD_WITH_PARAMS_JOB_ENDPOINT = 'job/%s/buildWithParameters' % job_name
+        if parameters:
+            self.http_post(BUILD_WITH_PARAMS_JOB_ENDPOINT, query_data=parameters)
+        else:
+            self.http_post(BUILD_JOB_ENDPOINT)
+
+    def stop_build(self, job_name, number):
+        """
+        停止Jenkins build
+        :param job_name:
+        :param number:
+        :return:
+        """
+        STOP_BUILD_ENDPOINT = 'job/%s/%s/stop' % (job_name, number)
+        self.http_post(STOP_BUILD_ENDPOINT)
+
+    def get_build_console_output(self, job_name, number):
+        """
+        获取Jenkins build日志
+        :param job_name:
+        :param number:
+        :return:
+        """
+        BUILD_CONSOLE_OUTPUT_ENDPOINT = 'job/%s/%d/consoleText' % (job_name, number)
+        resp = self.http_get(BUILD_CONSOLE_OUTPUT_ENDPOINT)
+        return resp.text
+
+    def delete_build(self, job_name, number):
+        """
+        删除Jenkins build
+        :param job_name:
+        :param number:
+        :return:
+        """
+        DELETE_BUILD_ENDPOINT = 'job/%s/%s/doDelete' % (job_name, number)
+        self.http_post(DELETE_BUILD_ENDPOINT)
+
+    def get_queue_info(self, depth=0):
+        """
+        获取排队信息
+        :return: list of job dictionaries,
+        """
+        Q_INFO_ENDPOINT = 'queue/api/json'
+        resp = self.http_get(Q_INFO_ENDPOINT, query_data={'depth': depth})
+        return resp.json()['items']
+
+    def get_queue_item(self, item_number, depth=0):
+        """
+        Get information about a queued item (to-be-created job).
+        :param item_number:
+        :param depth:
+        :return:
+        """
+        Q_ITEM_ENDPOINT = 'queue/item/%d/api/json' % item_number
+        resp = self.http_get(Q_ITEM_ENDPOINT, query_data={'depth': depth})
+        return resp.json()
+
+    def cancel_queue(self, id):
+        """
+        取消排队任务
+        :param id:
+        :return:
+        """
+        CANCEL_QUEUE_ENDPOINT = 'queue/cancelItem'
+        try:
+            self.http_post(CANCEL_QUEUE_ENDPOINT, query_data={'id': id})
+        except Exception:
+            pass
